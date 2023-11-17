4d1b33fa0921b8a3d42aa116ca32402f56787a93
Shashank <shashankyadav4858@gmail.com>
2023-11-17 13:29:48 +0000

    Added split_method

--- src/model/train.py
+++ src/model/train.py
@@ -7,22 +7,29 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import mlflow.sklearn
 
 
-# define functions
-def main(args):
-    # TO DO: enable autologging
-
-
+# define functions to call from outside
+def train(training_data, reg_rate):
     # read data
-    df = get_csvs_df(args.training_data)
+    df = get_csvs_df(training_data)
 
     # split data
     X_train, X_test, y_train, y_test = split_data(df)
 
     # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    train_model(reg_rate, X_train, X_test, y_train, y_test)
 
+
+
+def main(args):
+    # TO DO: enable autologging
+    mlflow.sklearn.autolog()
+
+    # train model
+    train(args.training_data, args.reg_rate)
 
 def get_csvs_df(path):
     if not os.path.exists(path):
@@ -34,6 +41,11 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(df, 0.8, random_state=1)
+    # Return the split data
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
@@ -59,16 +71,8 @@ def parse_args():
 
 # run script
 if __name__ == "__main__":
-    # add space in logs
-    print("\n\n")
-    print("*" * 60)
-
     # parse args
     args = parse_args()
 
     # run main function
     main(args)
-
-    # add space in logs
-    print("*" * 60)
-    print("\n\n")
